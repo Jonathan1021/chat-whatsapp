03c2611d5584fd25b8e2e8e43751f2601183b4a4
Jonathan <jonathan.vega@pragma.com.co>
2025-12-04 15:36:12 -0500

    chore: remove environment files from git tracking

--- my-chat/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://8du2nts7lf.execute-api.us-east-1.amazonaws.com/dev',
-  wsUrl: 'wss://r1fz3mp7m6.execute-api.us-east-1.amazonaws.com/dev',
-  userPoolId: 'us-east-1_7kP8zYaS0',
-  userPoolClientId: '37bo1j2dbik9c4d6ae5eep72s',
-  region: 'us-east-1'
-};
